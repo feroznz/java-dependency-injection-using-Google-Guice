dffd83bc0178efe061fd5344d3893ec6195c5f2c
feroz <feroz.kurisi@cubane.co>
2019-07-08 17:58:12 +1000

    test the guice and order system

--- ordersystem/src/main/java/com/company/ordersystem/App.java
+++ ordersystem/src/main/java/com/company/ordersystem/App.java
@@ -1,13 +1,32 @@
 package com.company.ordersystem;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.company.ordersystem.di.OrderModule;
+import com.company.ordersystem.model.CoffeType;
+import com.company.ordersystem.model.Coffee;
+import com.company.ordersystem.model.Ingredient;
+import com.company.ordersystem.model.Size;
+import com.company.ordersystem.service.OrderService;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class App {
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new OrderModule());
+
+        /*    Build object using injector*/
+        OrderService orderService = injector.getInstance(OrderService.class);
+
+        Coffee espresso = new Coffee(CoffeType.ESPRESSO, Ingredient.NONE, Size.REGULAR);
+        Coffee largeCappucino = new Coffee(CoffeType.CAPPUCCINO, Ingredient.NONE, Size.LARGE);
+        Coffee soyFlatWhite = new Coffee(CoffeType.FLAT_WHITE, Ingredient.SOY_MILK, Size.REGULAR);
+        Coffee cappuccino = new Coffee(CoffeType.CAPPUCCINO, Ingredient.NONE, Size.REGULAR);
+
+        orderService.addToOrder(espresso);
+        orderService.addToOrder(largeCappucino);
+        orderService.addToOrder(soyFlatWhite);
+        orderService.addToOrder(cappuccino);
+
+        System.out.println("Total for order: " + orderService.orderTotal());
+        System.out.println("Total GST: " + orderService.calculateGST());
     }
 }
